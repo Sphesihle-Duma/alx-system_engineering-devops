8a27139e6c35e90be9adcfa7b079b195358502b3
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-09-25 11:21:58 -0700

    Added the first task of the puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Creates a file in /tmp and file is school
+
+file { '/tmp/school'
+  ensure => file,
+  path   => '/tmp/school',
+  mode   => '0744',
+  owner  => 'www-data',
+  group  => 'www-data',
+  content => 'I love Puppet'
+}
