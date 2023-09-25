dd966d1cc50806a59882815dd636e1109487549e
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-09-25 11:45:28 -0700

    testing

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# Creates a file in /tmp and file is school
+# Creates a file /tmp/school
 
 file { '/tmp/school':
-  ensure => file,
-  path   => '/tmp/school',
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
