9b695ecf636b11a7d2222c65b78bdfd3d1192513
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-09-25 11:28:16 -0700

    fixing the colon issue

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Creates a file in /tmp and file is school
 
-file { '/tmp/school'
-  ensure => present,
+file { '/tmp/school':
+  ensure => file,
   path   => '/tmp/school',
   mode   => '0744',
   owner  => 'www-data',
